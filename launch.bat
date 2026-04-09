58226cd7d1eaa5deefb15e6288dbd757e2e46704
SirChris50 <118955855+SirChris50@users.noreply.github.com>
2026-04-09 08:51:24 -0500

    fix launch.bat shortcut

---
+++ launch.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" /min cmd /c "timeout /t 4 /nobreak >nul && start "" http://localhost:5000"
+cmd /k "cd /d D:\Chatman_Retrieval && call retrieval_env\Scripts\activate.bat && python app/app.py"
